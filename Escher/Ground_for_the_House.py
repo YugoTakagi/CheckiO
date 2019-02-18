0d2155eaa606e7ce1317c0093df163c3851a33c1
高城友豪 <a217048@ns.kogakuin.ac.jp>
2019-02-18 15:31:30 +0900

    Ground_for_the_Houseを追加

--- Escher/Ground_for_the_House.py
+++ Escher/Ground_for_the_House.py
@@ -1,45 +1,68 @@
-import numpy as np
+####note#############################################
+#def house(plan):
+#    grounds = [(i, j) for i, row in enumerate(plan.split()) for j, cell in enumerate(row) if cell == '#']
+#    if not grounds:
+#        return 0
+#    xs, ys = zip(*grounds)
+#    return (xs[-1] - xs[0] + 1) * (max(ys) - min(ys) + 1)
+#
+#
 def house(plan):
+    if '#' not in plan:
+        return 0
+    grid = plan.split()
+    no_so, east, west = [], [], []
+    for index, line in enumerate(grid):
+        if '#' in line:
+            no_so.append(index)
+            east.append(line.find('#'))
+            west.append(line.rfind('#'))
+    return (max(no_so) - min(no_so) + 1) * (max(west) - min(east) + 1)
+#grid = plan.split() == grid = plan.split('\n')
+def note():
+    l=[1,2,3,4,5]
+    for i, value in enumerate(l):#enumerate(list) -> index, value?
+        print("i, value = {}, {}".format(i,value))
+    for i, value in enumerate(l, 22):#enumerate(list) -> index, value?
+        print("i, value = {}, {}".format(i,value))
+#####################################################
+import numpy as np
+def vhouse(plan):
+    pass
+def myhouse(plan):
     #replace this for solution
-    print(plan)
-    h=0
-    w=0
-    row=[]
-    col=[]
-    i = [i for i in range(len(plan)) if plan[i]=='\n']
-    wb = i[1]-i[0]
-    for line in range(len(i)):
-        l = plan[wb *line:wb *(line +1)]
-        li = list(l)
-        row.append([val for val in li if val!="\n"])
-    
-    print(row)
-    for i in range(len(row)):
-        for j in range(len(row[i])):
-            if row[i][j]=='#':
-                h +=1
+    h,w=[],[]
+    splan = plan.split('\n')
+    nsplan=[list(splan[i]) for i in range(len(splan)) if splan[i] != '']
+    nplist=np.array(nsplan)
+    for i in range(len(nplist)):
+        for j in range(len(nplist[i])):
+            if nplist[i][j] == "#":
+                h.append(i)
                 break
-    array = np.array(row)
-    print(array)
-    print(wb -1)
-    if array[0] == []:
-        array = np.delete(array, 0, 0)
-    for i in range(wb-1):
-        print(array[:,i])
-        col.append(array[:,i])
-    for i in range(len(col)):
-        for j in range(len(col[i])):
-            if col[i][j]=='#':
-                w +=1
+    for i in range(len(nplist[0])):
+        col = nplist[:,i]
+        for j in range(len(col)):
+            if col[j]=='#':
+                w.append(i)
                 break
-    print(w*h)
-    return h*w
-
+    if len(h) != 0:
+        nh = [i for i in range(h[0],h[len(h)-1] +1,1)]
+    else:
+        nh = []
+    if len(w) != 0:
+        nw = [i for i in range(w[0],w[len(w)-1] +1,1)]
+    else:
+        nw = []
+    return len(nh)*len(nw)
 if __name__ == '__main__':
-	house('''
-0000000
-##00##0
-######0
-##00##0
-#0000#0
+    note()
+    s1 = house('''
+0##0
+0000
+#00#
 ''')
+    s = vhouse('''0000
+0000
+''')
+    print(s1)
